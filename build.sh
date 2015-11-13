a48f794c8e688b0528b0be87301b1739307a6503
cosigma <aircut@hotmail.de>
2015-11-13 15:37:38 +0100

    server partial implementation

--- build.sh
+++ build.sh
@@ -7,13 +7,28 @@ if [ -f "start" ]; then
 	rm start
 fi
 
+if [ -f "server" ]; then
+	rm server 
+fi
+
 gcc main.c \
 	-Wall \
 	-o bin/start \
 	-std=c99
 
+gcc server.c \
+	-Wall \
+	-o bin/server \
+	-std=c99
+
 if [ -f "bin/start" ]; then
 	chmod 755 bin/start
+else
+	echo "File start was not created"
+fi
+
+if [ -f "bin/start" ]; then
+	chmod 755 bin/server
 else
 	echo "File start was not created"
 fi
