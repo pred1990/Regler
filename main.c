2bc782fdec36de51b49e3685900900bdc658f381
msk <steffenkin@hotmail.de>
2015-11-17 13:33:21 +0100

    restructure network code

--- main.c
+++ main.c
@@ -6,39 +6,31 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h> 
+#include <fcntl.h>
 #include "number_name_defs.h"
 
 void interpret_all(int32, char**);
 int32 index_of(char*, char);
 
+//network
+int32 client_connect(char* address, uint32 port, int32* socket_handle);
+int32 socket_unblock_io(int32 socket_handle);
+
 int32 main(int32 argL, char** argV){
   uint32 port = 4242;
+  int32 socket_handle = 0;
 
-  int32 socket_handle = socket(AF_INET, SOCK_STREAM, 0);
+  int32 error = client_connect("127.0.0.1", port, &socket_handle);
+  if(error < 0)
+    printf("connection failed");
 
-  //NOTE returns 0 if host cannot be found or is malformed
-  struct hostent *server = gethostbyname("127.0.0.1");
-  if(server == 0)
-    return -1;
+  //NOTE when the connection is successfully established unblock the i/o
+  //socket_unblock_io(socket_handle);
 
-  struct sockaddr_in ip_address;
-	memset(&ip_address, 0 ,sizeof(ip_address));
-
-  //NOTE Use Inet, despise the fact that we are local
-  //NOTE htons translates the number to Network byte order
-  ip_address.sin_family = AF_INET;
-  ip_address.sin_port = htons(port);
-
-  if(connect(socket_handle, (struct sockaddr*) &ip_address, sizeof(ip_address)) < 0);
-    printf("connect error");
-  
   char * message = "test\n";
   write(socket_handle, message, sizeof(message));
 
   close(socket_handle);
-
-  //memcpy(&ip_address.sin_addr.s_addr, server->h_addr_list[0] , server->h_length);
-
 
 	//interpret_all(argL, argV);
 	return 0;
@@ -60,4 +52,50 @@ int32 index_of(char* search_str, char find_char){
 		++i;
   	}
 	return search_str[i] ? -1 : i;
+}
+
+int32 socket_unblock_io(int32 socket_handle){
+  //get current config of socket
+  int32 current_flags = fcntl(socket_handle, F_GETFL);
+  if(current_flags < 0){
+    return -1;
+  }
+
+  //wirteback config with non block
+  int32 error = fcntl(socket_handle, F_GETFL, current_flags | O_NONBLOCK);
+  if(error < 0){
+    return -1;
+  }
+  return 0;
+}
+
+int32 client_connect(char * address, uint32 port, int32* socket_handle){
+
+  //get socket
+  *socket_handle = socket(AF_INET, SOCK_STREAM, 0);
+
+
+  //NOTE returns 0 if host cannot be found or is malformed
+  struct hostent *server = gethostbyname(address);
+  if(server == 0)
+    return -1;
+
+
+  struct sockaddr_in ip_address;
+  memset(&ip_address, 0 ,sizeof(ip_address));
+
+
+  //NOTE htons translates the number to Network byte order
+  ip_address.sin_family = AF_INET;
+  ip_address.sin_port = htons(port);
+
+
+  printf("connecting to: %s\n", address);
+  int32 state = connect(  *socket_handle,
+                          (struct sockaddr*) &ip_address,
+                          sizeof(ip_address));
+  if(state < 0)
+    return -1;
+
+  return 0;
 }
